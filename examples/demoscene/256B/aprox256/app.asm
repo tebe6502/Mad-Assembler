ef8eaa3347f663690fb6397dbf25a55cd6eb696e
tebe6502 <tebe6502@gmail.com>
2024-09-12 16:19:33 +0200

    aprox256

---
+++ examples/demoscene/256B/aprox256/app.asm
@@ -0,0 +1,174 @@
+
+	org $2000
+
+x0 = $CB
+x1 = $CC
+x2 = $CD
+x3 = $CE
+vl = $CF
+dvl = $D0
+vr = $D1
+dvr = $D2
+tt = $D3
+
+; 0  1
+; 2  3
+;setup chars
+	lda #$80
+	sta 623 ; GPRIOR
+	sta 756
+	ldx #0
+	stx $D5
+	ldy #0
+chlp
+	txa
+	sta $8000,y
+	iny
+	tya
+	and #7
+	bne chlp
+	txa
+	clc
+	adc #$11
+	tax
+	bcc chlp
+
+; copy colors
+	ldx #8
+copc
+	lda colors-1,x
+	sta 704,x
+	dex
+	bne copc
+; init vars
+ivar
+	ldx #4
+iv	txa
+	eor $14
+	adc 53770
+	sta x0-1,x
+	dex
+	bne iv
+
+	lda #$50
+	sta $D208
+lp1
+
+	lda x0
+	sta vl
+
+	lda x1
+	sta vr
+
+	lda 88
+	sta $EB
+	lda $BC25
+	eor #$0C
+	sta $EC
+
+	pha
+	ldx #24
+
+ylp	ldy #0
+	lda x2
+	sec
+	sbc vl
+	jsr sign
+	clc
+	adc vl
+	sta vl
+	sta tt
+	lda x3
+	sec
+	sbc vr
+;	jsr sign
+	clc
+	adc vr
+	sta vr
+
+xlp
+	lda vr
+	sec
+	sbc vl
+	jsr sign
+	clc
+	adc tt
+	sta tt
+	and #$0F
+	cmp #$08
+	bcs noin
+	eor #$FF
+noin
+	and #$07
+	sta ($EB),y
+	iny
+	cpy #40
+	bne xlp
+	tya
+	clc
+	adc $EB
+	sta $EB
+	bcc ny
+	inc $EC
+ny	dex
+	bne ylp
+;	stx $D201
+	inc x0
+; fade sound ?
+	lda $D5
+	beq nofas
+	sec
+	sbc #8
+	bcs nofas
+;	jmp *
+	lda #0
+nofas sta $D5
+	sta $D201
+	lda $14
+	bne bra1
+; change colors
+	lda $13
+	asl
+	asl
+	asl
+	asl
+	sta $d4
+	ldx #8
+;	ldy #0
+co1 lda colors-1,x
+	and #$0F
+	ora $D4
+	sta colors-1,x
+	sta 704,x
+	dex
+	bne co1
+	lda #$CF
+	sta $D200
+	lda #$2F
+	sta $D201
+	sta $D5
+	dec x3
+bra1
+
+;wait_frame
+RTCLOK=$0012
+      lda RTCLOK+2
+waits
+      cmp RTCLOK+2
+       beq waits
+	pla
+	sta $BC25
+
+	jmp lp1
+sign
+	beq zero
+	bpl plus
+	lda #$FF
+zero
+	rts
+plus
+	lda #1
+	rts
+colors
+;	.byte $1F,$1D,$1B,$19,$17,$15,$13,$11
+	.byte $10,$12,$14,$16,$18,$1A,$1C,$1E
